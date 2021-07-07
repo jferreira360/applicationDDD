976d419f667fd032750f288d5c78f58bac1fbeef
jferreira360 <jferreirabenedette@gmail.com>
2021-07-06 23:21:23 -0300

    Criando a entidade produto

---
+++ NerdStore/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.DomainObjects
+{
+    /// <summary>
+    /// Classe base de toda entidade
+    /// </summary>
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        
+        /// <summary>
+        /// Uma entidade é igual a outra quando são do mesmo tipo e possuem o mesmo Id
+        /// Se o Id Mudar, temos duas entidaddes diferentes, embora sejam do mesmo tipo
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+            return Id.Equals(compareTo.Id);
+        }
+
+
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {            
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+    }
+}
